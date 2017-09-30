fe7b6633cedf8f2518c77762e4a81a4e00aa4dc6
root <root@DESKTOP-5G1VC6U.localdomain>
2017-09-30 17:31:36 +0100

    password

--- FileEncryptor.java
+++ FileEncryptor.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -8,48 +9,80 @@ import java.nio.file.StandardOpenOption;
 import java.util.Base64;
 import java.util.Base64.Decoder;
 import java.util.Base64.Encoder;
+import java.util.Random;
 
 @SuppressWarnings("unused")
 public class FileEncryptor {
-		
-	private File  file;
-	
-	public  FileEncryptor(File file) {
+
+	private File file;
+
+	public FileEncryptor(File file) {
 		this.setFile(file);
 	}
-	
-	public byte [] Encrypt() throws IOException {
-		//this shit is hot.
+
+	public byte[] Encrypt(String password) throws IOException {
+		// this shit is hot.
 		Encoder base = Base64.getEncoder();
 		Path path = Paths.get(file.getAbsolutePath());
 		byte[] data = Files.readAllBytes(path);
-		byte [] encoded = base.encode(data);
-		for(int i=0; i<encoded.length; i++) {
-			encoded[i]+=(byte) 256;
+		byte[] encoded = base.encode(data);
+		byte[] passbytes = password.getBytes();
+		for (int i = 0; i < encoded.length; i++) {
+			for (int j = 0; j < passbytes.length; j++) {
+				encoded[i] += passbytes[j];
+			}
 		}
-		byte [] doubleEncoded = base.encode(encoded);
+		byte[] doubleEncoded = base.encode(encoded);
 		return doubleEncoded;
 	}
-	
-	public byte [] Dencrypt() throws IOException {
-	Decoder decoded = Base64.getDecoder();
-	Path path = Paths.get(file.getAbsolutePath());
-	byte[] data = Files.readAllBytes(path);
-	byte [] encoded = decoded.decode(data);	
-	for(int i=0; i<encoded.length; i++) {
-		encoded[i]-=(byte) 256 ;
+
+	public byte[] Dencrypt(String password) throws IOException {
+		Decoder decoded = Base64.getDecoder();
+		Path path = Paths.get(file.getAbsolutePath());
+		byte[] data = Files.readAllBytes(path);
+		byte[] encoded = decoded.decode(data);
+		byte[] passbytes = password.getBytes();
+		for (int i = 0; i < encoded.length; i++) {
+			for (int j = 0; j < passbytes.length; j++) {
+				encoded[i] -= passbytes[j];
+			}
+		}
+
+		byte[] doubledecoded = decoded.decode(encoded);
+		return doubledecoded;
+
 	}
-	byte [] doubledecoded = decoded.decode(encoded);
-	return doubledecoded;
-	
+
+	public char charGen() throws UnsupportedEncodingException {
+		Random ran = new Random();
+		int c = ran.nextInt(126);
+		if (c <= 32) {
+			c += 32;
+		}
+
+		byte b = (byte) c;
+		byte[] bytes = new byte[1];
+		bytes[0] = b;
+		System.out.println("letter " + b);
+		String chars = new String(bytes, "UTF-8");
+		char[] chararray = chars.toCharArray();
+		return chararray[0];
+
 	}
-	
-	
-	public void writeFile(byte [] data) throws IOException {
-		Files.write(Paths.get(file.getAbsolutePath()+"2"), data, StandardOpenOption.CREATE_NEW);
-	
+
+	public String passwordGen() throws UnsupportedEncodingException {
+		StringBuilder password = new StringBuilder();
+		for (int i = 0; i < 256; i++) {
+			password.append(charGen());
+		}
+		System.out.println(password.toString());
+		return password.toString();
 	}
-	
+
+	public void writeFile(byte[] data) throws IOException {
+		Files.write(Paths.get(file.getAbsolutePath() + "2"), data, StandardOpenOption.CREATE_NEW);
+
+	}
 
 	public File getFile() {
 		return file;
@@ -58,6 +91,5 @@ public class FileEncryptor {
 	public void setFile(File file) {
 		this.file = file;
 	}
-	
 
 }
--- UI.java
+++ UI.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -8,24 +9,22 @@ import javax.swing.JFrame;
 public class UI extends JFrame {
 
 	public UI () {
-		JFrame frame=new JFrame();
-		frame.setSize(400,250);
-		frame.setVisible(true);
-		frame.setTitle("Mans not hot");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		JButton a=new JButton("Encrypt");
-		JButton b=new JButton("Decrypt");
-		a.setBounds(50,50,90, 50);
-		b.setBounds(50,50,90, 50); 
-		frame.add(a);
-		frame.add(b);
-		
+	
 	}
 	
 	public static void main(String[] args) {
-	new UI();
-	
+		 FileEncryptor file = new  FileEncryptor(new File("C:\\Users\\Ricky\\Desktop\\test.txt2"));
+		 try {
+			byte [] data = file.Dencrypt("J${`^wO\"a )5VmM;<dF>!CgmHj-0uOA8[5:4^2BGdh'\\A.==rWS5>HoKO}Nx\"hx{/JzZ.8,)`,90oF;}( @1qZ!2%O%G80\"GdB.Q6.,|hPNI-Vw^b09W=edn<H3v'*+M^%3n|!0ih5zP9&5&^x,ON;O46(1IXcH:M=`=@<t<W8Y<I=w>:>BAy4TQ%%\"0oYd?&wc+*)!ucIa=A6FxVDD.t+5?12(pUdx1R+RD<Y<?0_k{m#L>8v@6IdTt%&LBP$x4J${`^wO\"a )5VmM;<dF>!CgmHj-0uOA8[5:4^2BGdh'\\A.==rWS5>HoKO}Nx\"hx{/JzZ.8,)`,90oF;}( @1qZ!2%O%G80\"GdB.Q6.,|hPNI-Vw^b09W=edn<H3v'*+M^%3n|!0ih5zP9&5&^x,ON;O46(1IXcH:M=`=@<t<W8Y<I=w>:>BAy4TQ%%\"0oYd?&wc+*)!ucIa=A6FxVDD.t+5?12(pUdx1R+RD<Y<?0_k{m#L>8v@6IdTt%&LBP$x4");
+			file.writeFile(data);
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 
 }
 }
